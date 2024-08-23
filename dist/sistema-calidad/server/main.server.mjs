7b3b803d571d65932c331fcb042847a5a3f85ed5
IrcoIT <david.qg@ircosl.com>
2024-08-23 12:36:11 +0200

    version 1

--- dist/sistema-calidad/server/main.server.mjs
+++ dist/sistema-calidad/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LX24LQRP.mjs";import"./chunk-ONBLNN7T.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-GWF4KA4R.mjs";import"./chunk-ONBLNN7T.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
